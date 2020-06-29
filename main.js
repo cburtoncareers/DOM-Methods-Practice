6331abac7d0fa78dc1c0b49cffb1e9365a036c79
Nita <cburtoncareers@yahoo.com>
2020-06-29 12:27:17 -0400

    Changed the img element

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 const parentElement = document.querySelector('body');
 const removeButtonElement = document.createElement('button');
 const mainElement = document.createElement('main')
-const imageElement = document.createElement('image')
+const imageElement = document.createElement('img')
 const anchorElement = document.createElement('a')
 
 
@@ -14,7 +14,7 @@ mainElement.append(imageElement, anchorElement)
 
 //Set the attributes for the image tag
 
-imageElement.setAttribute('class', 'image')
+imageElement.setAttribute('class', 'img')
 imageElement.setAttribute('alt', 'GWagon')
 imageElement.setAttribute('src', 'https://i.pinimg.com/564x/3b/d4/f8/3bd4f83d0c3cc87b9a78b84b17fc4625.jpg')
 
